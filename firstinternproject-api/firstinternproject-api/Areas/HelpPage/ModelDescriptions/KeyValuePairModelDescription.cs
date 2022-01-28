f1f837247e9760a84e65c7d7375c32b5866689f7
NicolaZBourbaki <sahlofolina2211@gmail.com>
2022-01-28 15:20:09 +0200

    .net6 asp net app

--- firstinternproject-api/firstinternproject-api/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace firstinternproject_api.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
