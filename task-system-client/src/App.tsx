89caf9fa8ae348a39ad3a6c28b5e4fed04017feb
NikolaZBourbaki <sahlofolina2211@gmail.com>
2022-02-01 19:12:42 +0200

    Created sign up log in pages

--- task-system-client/src/App.tsx
+++ task-system-client/src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
+import { Routes, Route, HashRouter } from "react-router-dom";
+import Navbar from './Components/navbar/Navbar';
+import Login from './Components/register/Login';
+import SignUp from './Components/register/SignUp';
 
 function App() {
   return (
     <div className='App'>
       <Navbar></Navbar>
+      <HashRouter>
+        <Routes>
+        <Route path="/" element={Navbar}></Route>
+          <Route path="/login" element={Login}></Route>
+          <Route path="/signup" element={SignUp}></Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
-}
+};
 
 export default App;
